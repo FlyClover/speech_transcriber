ab4d0b2ae964e36d36dafe4c860f822965a42d54
Chaofei Fan <cfan@mobvoi.com>
2018-04-19 15:21:54 +0800

    Update interface

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # speech_transcriber
-
+```
 mvn compile
 mvn exec:java -Dexec.mainClass=test.SpeechTest -Dexec.args=/path/to/wav
+```
--- src/main/java/test/SpeechTest.java
+++ src/main/java/test/SpeechTest.java
@@ -17,53 +17,54 @@ import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 
 public class SpeechTest {
-  public static void main(String... args) throws Exception {
-    Logger logger = Logger.getLogger(SpeechTest.class.getName());
+    public static void main(String... args) throws Exception {
+        Logger logger = Logger.getLogger(SpeechTest.class.getName());
 
-    if (args.length == 0) {
-      logger.warning("Specify input wav path");
-      return;
+        if (args.length == 0) {
+            logger.warning("Specify input wav path");
+            return;
+        }
+
+        ManagedChannel channel =
+                ManagedChannelBuilder.forAddress("jenkins-slave", 32769).usePlaintext(true).build();
+        SpeechGrpc.SpeechBlockingStub blockingStub = SpeechGrpc.newBlockingStub(channel);
+
+        String fileName = args[0];
+        logger.info(fileName);
+        Path path = Paths.get(fileName);
+        byte[] data = Files.readAllBytes(path);
+        ByteString audioBytes = ByteString.copyFrom(data);
+
+        SpeechProto.RecognitionAudio audio =
+                SpeechProto.RecognitionAudio.newBuilder().setContent(audioBytes).build();
+
+        SpeechProto.RecognitionConfig config =
+                SpeechProto.RecognitionConfig.newBuilder()
+                        .setEncoding(SpeechProto.RecognitionConfig.Encoding.WAV16)
+                        .setSampleRate(8000)
+                        .setChannel(1)
+                        .build();
+
+        SpeechProto.RecognizeRequest request =
+                SpeechProto.RecognizeRequest.newBuilder().setConfig(config).setAudio(audio).build();
+
+        SpeechProto.RecognizeResponse response;
+        try {
+            response = blockingStub.recognize(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+            return;
+        }
+
+        for (SpeechProto.SingleUtteranceResult result : response.getResultsList()) {
+            SpeechProto.SpeechRecognitionAlternative alternative = result.getAlternatives(0);
+
+            logger.log(Level.INFO, Float.toString(result.getStartTime()) + "-"
+                            + Float.toString(result.getEndTime()) + " "
+                            + alternative.getTranscript());
+        }
+
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
-
-    ManagedChannel channel = ManagedChannelBuilder
-            .forAddress("localhost", 32768)
-            .usePlaintext(true)
-            .build();
-    SpeechGrpc.SpeechBlockingStub blockingStub = SpeechGrpc.newBlockingStub(channel);
-
-    String fileName = args[0];
-    logger.info(fileName);
-    Path path = Paths.get(fileName);
-    byte[] data = Files.readAllBytes(path);
-    ByteString audioBytes = ByteString.copyFrom(data);
-
-    SpeechProto.Audio audio = SpeechProto.Audio.newBuilder()
-            .setEncoding(SpeechProto.Audio.Encoding.WAV16)
-            .setSampleRate(8000)
-            .setChannel(2)
-            .setContent(audioBytes)
-            .build();
-    SpeechProto.RecognizeRequest request = SpeechProto.RecognizeRequest.newBuilder()
-            .setAudio(audio)
-            .build();
-
-    SpeechProto.RecognizeResponse response;
-    try {
-      response = blockingStub.recognize(request);
-    } catch (StatusRuntimeException e) {
-      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-      channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-      return;
-    }
-
-    for (SpeechProto.SingleUtteranceResult result : response.getResultsList()) {
-      SpeechProto.SpeechRecognitionAlternative alternative = result.getAlternatives(0);
-
-      logger.log(Level.INFO, Float.toString(result.getStartTime()) + "-" +
-              Float.toString(result.getEndTime()) + " " +
-              alternative.getTranscript());
-    }
-
-    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-  }
 }
